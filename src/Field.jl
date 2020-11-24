a2e1bc78851a32e37bb0206f6a86873f0449d251
Philipp Haim <philipp.haim@gmail.com>
2020-11-24 01:34:27 +0100

    Broadcasting for Fields, new Adjoint type-parameter

--- src/Field.jl
+++ src/Field.jl
@@ -1,48 +1,56 @@
-import Base.size, Base.length, Base.getindex, Base.setindex!, Base.eltype, Base.iterate
-import Base.randn, Base.eachindex, Base.similar
+import Base.size, Base.getindex, Base.setindex!, Base.IndexStyle
+import Base.randn, Base.similar
 
 include("Domain.jl")
 
-#abstract type AbstractField{T, N} <: AbstractArray{T, N} end
-abstract type AbstractField{T, N} end
-abstract type AbstractMultiField end
-abstract type Linearization end
-
-
-struct Field{T, N, Dom} <: AbstractField{T, N} where Dom <: AbstractDomain
+struct Field{A, T, N, Dom} <: AbstractArray{T, N} where Dom <: AbstractDomain
     domain::Dom
     val::Array{T, N}
 end
 
-field(domain, val) = begin
+field(domain, val, adjoint=false) = begin
     @assert size(domain) == size(val)
-    Field{eltype(val), dims(domain), typeof(domain)}(domain, val)
+    Field{adjoint, eltype(val), dims(domain), typeof(domain)}(domain, val)
 end
 
-struct MultiField <: AbstractMultiField
-    domains::Tuple
-    vals::Tuple
+struct FieldStyle{A, N, Dom} <: Broadcast.AbstractArrayStyle{N} end
+Base.BroadcastStyle(::Type{<:Field{A, T, N, Dom}}) where {A, T, N, Dom} = FieldStyle{A, N, Dom}()
+Base.BroadcastStyle(::FieldStyle{A, N, Dom}, ::FieldStyle{A, N, Dom}) where {A, N, Dom} = FieldStyle{A, N, Dom}()
+#Base.BroadcastStyle(::Broadcast.Style{<:Number}, ::FieldStyle{A, N, Dom}) where {A, N, Dom} = FieldStyle{A, N, Dom}()
+#Base.BroadcastStyle(::FieldStyle{A, N, Dom}, ::Broadcast.Style{<:Number}) where {A, N, Dom} = FieldStyle{A, N, Dom}()
+
+function similar(bc::Broadcast.Broadcasted{FieldStyle{A, N, Dom}}, ::Type{ElType}) where {A, N, Dom, ElType}
+    domain = getdomain(bc)
+    return field(domain, similar(Array{ElType}, axes(bc)), A)
 end
 
+getdomain(bc::Base.Broadcast.Broadcasted) = getdomain(bc.args)
+getdomain(args::Tuple) = getdomain(args[1])
 getdomain(f::Field) = f.domain
 
+is_adjoint(f::Field{A}) where A = A
 
 #Use Metaprogramming here!
-size(f::Field) = size(f.val)
-length(f::Field) = prod(size(f))
+size(f::Field) = Int.(size(f.val))
+IndexStyle(::Type{<:Field}) = IndexLinear()
+
 getindex(f::Field, i::Integer) = getindex(f.val, i)
 getindex(f::Field, I::Vararg{Integer, N}) where N = getindex(f.val, I...)
 setindex!(f::Field, v, i::Integer) = setindex!(f.val, v, i)
 setindex!(f::Field, v, I::Vararg{Integer, N}) where N = setindex!(f.val, v, I...)
-eltype(f::Field) = eltype(f.val)
 
-similar(f::Field) = field(f.domain, similar(f.val))
-similar(f::Field, dom::AbstractDomain) = field(dom, similar(f.val))
 
-eachindex(f::Field) = eachindex(f.val)
+similar(f::Field) = similar(f, eltype(f), size(f))
+similar(f::Field, ::Type{T}, ::Tuple{Vararg{Int64, N}}) where {T, N} = field(f.domain, similar(f.val, T))
+#similar(f::Field) = field(f.domain, similar(f.val))
+#similar(f::Field, dom::AbstractDomain) = field(dom, similar(f.val))
+
+
 randn(d::AbstractDomain) = field(d, randn(size(d)...))
 rand(d::AbstractDomain) = field(d, rand(size(d)...))
 
 function pindices(ps::Field{T, N, PD}) where {T, N, PD <: PowerDomain}
     return ps.domain._pindices
 end
+
+#MultiField: named tuples of fields
